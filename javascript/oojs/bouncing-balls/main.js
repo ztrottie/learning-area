e5a3a8e15bc39a1fa1d645132350346757bdcf62
Francesca Sandford <114738789+fransan6@users.noreply.github.com>
2023-08-21 14:12:06 +0100

    Add brackets to match webpage in JavaScript -> Object building practice (#648)
    On the [web
    page](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_building_practice#getting_started),
    there are brackets so I have added brackets to the downloadable js file.
    Whether the webpage or downloadable file is updated, I believe they
    should be consistent.
    
    Co-authored-by: Brian Thomas Smith <brian@smith.berlin>

--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -3,8 +3,8 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
 
